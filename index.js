a976e074b38e6b573ec513eef495ceed63b7575e
lonzolyn <99560335+lonzolyn@users.noreply.github.com>
2023-05-08 13:51:16 -0400

    addin a little bit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-require('dotenv').config()
 const express = require('express')
+require('dotenv').config()
 const app = express()
 
 app.set('view engine', 'jsx')
@@ -7,8 +7,8 @@ app.engine('jsx', require('express-react-views').createEngine)
 
 app.use('/places',require('./controllers/places'))
 
-app.get('/', (req, res) => {
-    res.render('home')
+app.get('*', (req, res) => {
+    res.render('error404')
 })
 app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
