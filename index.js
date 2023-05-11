678faf98280b13e05e1f6e879bea633af92cb3d8
lonzolyn <99560335+lonzolyn@users.noreply.github.com>
2023-05-11 09:55:16 -0400

    adding 404 page view

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine)
+app.engine('jsx', require('express-react-views').createEngine())
 
 app.use('/places',require('./controllers/places'))
-
+app.use('/about',require('./controllers/places'))
 app.get('*', (req, res) => {
     res.render('error404')
 })
-app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
-})
+//app.get('*', (req, res) => {
+  //  res.status(404).send('<h1>404 Page</h1>')
+//})
 
 app.listen(process.env.PORT)
---
+++ views/about.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const Def = require('./default')
+
+function about () {
+    return (
+        <Def>
+            <main>
+                <h1>About</h1>
+            </main>
+        </Def>
+    )
+}
+
+module.exports = about
