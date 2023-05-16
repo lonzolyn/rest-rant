c77f187b748d1e2102f4f492c82c440af2506b25
lonzolyn <99560335+lonzolyn@users.noreply.github.com>
2023-05-16 15:03:34 -0400

    adding stype to Rest-Rant

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 
+//Express Settings
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+//Controllers and Routes
 app.use('/places',require('./controllers/places'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
 // app.use('/about',require('./controllers/places'))
 app.get('*', (req, res) => {
     res.render('error404')
